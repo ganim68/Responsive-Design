4329554cb8828de7496460f5f92d53c57d573457
Ganim <ganim68@gmail.com>
2017-10-24 21:56:59 +0300

    עיצוב בלוקים

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function myFunction() 
+    {
+       var x = document.getElementById("mymenu");
+       if (x.className === "menu") 
+        {
+            x.className += " responsive";
+        } 
+       else
+        {
+            x.className = "menu";
+        }
+    }
